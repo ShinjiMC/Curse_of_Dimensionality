42d9c95ac28b604cd7aea843f53bca084eac59f3
ShinjiMC <nayapbrau@gmail.com>
2023-09-19 19:50:35 -0500

    Upload 2.0v (fixed the golang)

--- Histogram/Histogram.go
+++ Histogram/Histogram.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -13,25 +14,50 @@ import (
 	"gonum.org/v1/plot/vg"
 )
 
-func SplitLogLine(text string) []float64 {
+func SplitLogLine(text string) (int, []float64) {
 	re := regexp.MustCompile(`\s+`)
 	parts := re.Split(text, -1)
 	var numbers []float64
-	for _, part := range parts {
+	var dims int
+	for i, part := range parts {
 		part = strings.TrimSpace(part)
 		if part != "" {
 			number, err := strconv.ParseFloat(part, 64)
 			if err != nil {
 				fmt.Printf("Error al analizar número '%s': %v\n", part, err)
 			} else {
-				numbers = append(numbers, number)
+				if i == 0 {
+					dims = int(number)
+				} else {
+					numbers = append(numbers, number)
+				}
 			}
 		}
 	}
-	return numbers
+	return dims, numbers
 }
 
-func Ploteo(distances []float64) {
+func getMaxValueX(distances []float64) int {
+	maxValue := math.Inf(-1)
+	for _, value := range distances {
+		if value > maxValue {
+			maxValue = value
+		}
+	}
+	return int(math.Ceil(maxValue))
+}
+
+func getMinValueX(distances []float64) int {
+	minValue := math.Inf(1)
+	for _, value := range distances {
+		if value < minValue {
+			minValue = value
+		}
+	}
+	return int(math.Floor(minValue))
+}
+
+func Ploteo(distances []float64, dim int) {
 
 	p := plot.New()
 
@@ -43,25 +69,47 @@ func Ploteo(distances []float64) {
 		log.Fatal(err)
 	}
 	p.Add(histogram)
-	p.X.Min = 0.0
-	p.X.Max = 14
-	p.Y.Min = 0
-	p.Y.Max = 1400
 
-	if err := p.Save(4.0*vg.Inch, 4.0*vg.Inch, "histogram.png"); err != nil {
+	maxX := float64(getMaxValueX(distances))
+	minX := float64(getMinValueX(distances))
+	p.X.Min = minX
+	p.X.Max = maxX
+	p.Y.Max = 1600
+	p.X.Label.Text = "Distancias"
+	p.Y.Label.Text = "Frecuencia"
+	p.Title.Text = fmt.Sprintf("Histograma de las distancias entre puntos con dimension %d", dim)
+	p.Y.Tick.Marker = plot.ConstantTicks([]plot.Tick{
+		{Value: 0.0, Label: "0"},
+		{Value: 200.0, Label: "200"},
+		{Value: 400.0, Label: "400"},
+		{Value: 600.0, Label: "600"},
+		{Value: 800.0, Label: "800"},
+		{Value: 1000.0, Label: "1000"},
+		{Value: 1200.0, Label: "1200"},
+		{Value: 1400.0, Label: "1400"},
+		{Value: 1600.0, Label: "1600"},
+	})
+	numDivisions := 8
+	xDivisions := make([]plot.Tick, numDivisions)
+	for i := 0; i < numDivisions; i++ {
+		value := minX + (float64(i) * (maxX - minX) / float64(numDivisions-1))
+		label := fmt.Sprintf("%.1f", value)
+		xDivisions[i] = plot.Tick{Value: value, Label: label}
+	}
+	p.X.Tick.Marker = plot.ConstantTicks(xDivisions)
+	if err := p.Save(6.0*vg.Inch, 6.0*vg.Inch, "histogram.png"); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Histograma guardado en 'histogram.png'")
 }
 
 func main() {
-	data, err := os.ReadFile("distances")
+	data, err := os.ReadFile("Histogram/distances")
 	if err != nil {
 		fmt.Println("Error reading pipe:", err)
 		return
 	}
 	//fmt.Println("Received:", string(data))
-	distances := SplitLogLine(string(data))
-	//fmt.Println(distances)
-	Ploteo(distances)
+	dim, distances := SplitLogLine(string(data))
+	Ploteo(distances, dim)
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <fstream>
 #include <cstdlib>
+#include <Windows.h>
 
 using namespace std;
 
@@ -42,31 +43,98 @@ std::vector<double> GetDistances(const std::vector<std::vector<double>>& points)
     return distances;
 }
 
-void LoadFile(const std::vector<double>& distances){
+void LoadFile(const std::vector<double>& distances, const int& dim){
     std::ofstream outputFile("Histogram/distances",std::ios::trunc);
     if (!outputFile.is_open()) {
         std::cerr << "Error al abrir el archivo para escritura." << std::endl;
         return;
     }
+    outputFile << dim << " ";
     for (size_t i = 0; i < distances.size(); ++i) {
         outputFile << distances[i] << " ";
     }
     outputFile.close();
 }
 
-int main(){
-    int d = 100;
-    std::vector<double> distances = GetDistances(GetPoints(d));
-    LoadFile(distances);
-    /*std::cout << "Distance: " << distances.size() << std::endl;
-    std::cout << "Datos de distances:" << std::endl;
-    for (size_t i = 0; i < distances.size(); ++i) {
-        std::cout << distances[i] << "\t";
-    }*/
-    int result = std::system(".\\Histogram\\Histogram.exe");
+int ChooseD() {
+    int opcion;
+    int valorSeleccionado = 0;
+    std::cout << "The Curse of Dimensionality" << std::endl;
+    while (true) {
+        std::cout << "Choose an option:" << std::endl;
+        std::cout << "1. 2 dimensions" << std::endl;
+        std::cout << "2. 10 dimensions" << std::endl;
+        std::cout << "3. 50 dimensions" << std::endl;
+        std::cout << "4. 100 dimensions" << std::endl;
+        std::cout << "5. 500 dimensions" << std::endl;
+        std::cout << "6. 1000 dimensions" << std::endl;
+        std::cout << "7. 2000 dimensions" << std::endl;
+        std::cout << "8. 5000 dimensions" << std::endl;
+        std::cout << "9. Exit" << std::endl;
 
-    if (result != 0) {
-        std::cerr << "Error al ejecutar el Ploteo en GO." << std::endl;
+        std::string input;
+        std::cout << "Input: ";
+        std::cin >> input;
+
+        if (input == "9") {
+            std::cout << "Exiting the program." << std::endl;
+            break;
+        }
+
+        try {
+            opcion = std::stoi(input);
+
+            switch (opcion) {
+                case 1:
+                    valorSeleccionado = 2;
+                    return valorSeleccionado;
+                case 2:
+                    valorSeleccionado = 10;
+                    return valorSeleccionado;
+                case 3:
+                    valorSeleccionado = 50;
+                    return valorSeleccionado;
+                case 4:
+                    valorSeleccionado = 100;
+                    return valorSeleccionado;
+                case 5:
+                    valorSeleccionado = 500;
+                    return valorSeleccionado;
+                case 6:
+                    valorSeleccionado = 1000;
+                    return valorSeleccionado;
+                case 7:
+                    valorSeleccionado = 2000;
+                    return valorSeleccionado;
+                case 8:
+                    valorSeleccionado = 5000;
+                    return valorSeleccionado;
+                default:
+                    std::cout << "Invalid option. Enter a number from 1 to 8 or 9 to exit." << std::endl;
+                    continue;
+            }
+        } catch (const std::invalid_argument& e) {
+            std::cout << "Invalid input. Enter a number from 1 to 8 or 9 to exit." << std::endl;
+        }
+    }
+    return valorSeleccionado;
+}
+
+
+int main(){
+    int d = ChooseD();
+    if ( d!=0 ) {
+        std::vector<double> distances = GetDistances(GetPoints(d));
+        LoadFile(distances,d);
+        int result = std::system(".\\Histogram\\Histogram.exe");
+        if (result != 0) {
+            std::cerr << "Error al ejecutar el Ploteo en GO." << std::endl;
+        }
+        std::remove(".\\Histogram\\distances");
+        int results = std::system(".\\histogram.png");
+        if (results != 0) {
+            std::cerr << "Error al abrir la imagen." << std::endl;
+        }
     }
     return 0;
 }
